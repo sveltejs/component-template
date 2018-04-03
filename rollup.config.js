60af5be139a918d0b45f53c8ffcfc673d4f4a112
Rich Harris <richard.a.harris@gmail.com>
2018-04-03 14:22:58 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,16 @@
 import svelte from 'rollup-plugin-svelte';
 import pkg from './package.json';
 
+const name = pkg.name
+	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+	.replace(/^\w/, m => m.toUpperCase())
+	.replace(/-\w/g, m => m[1].toUpperCase());
+
 export default {
 	input: 'src/index.html',
 	output: [
 		{ file: pkg.module, 'format': 'es' },
-		{ file: pkg.main, 'format': 'umd', name: 'SvelteComponent' }
+		{ file: pkg.main, 'format': 'umd', name }
 	],
 	plugins: [
 		svelte({

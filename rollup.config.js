65d5c8494d194d71f12cd9321bfc07b94391628b
Jacob Wright <jacwright@gmail.com>
2019-05-25 14:28:03 -0600

    Updates to v3
    Updates template for Svelte 3

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ const name = pkg.name
 	.replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.html',
+	input: 'src/index.svelte',
 	output: [
 		{ file: pkg.module, 'format': 'es' },
 		{ file: pkg.main, 'format': 'umd', name }

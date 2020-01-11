99ba12b700f84c9cec5d847fc826e214c6f29a0e
Conduitry <git@chor.date>
2020-01-11 10:15:21 -0500

    reorganize to more easily support multiple components

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ const name = pkg.name
 	.replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.svelte',
+	input: 'src/index.js',
 	output: [
 		{ file: pkg.module, 'format': 'es' },
 		{ file: pkg.main, 'format': 'umd', name }
